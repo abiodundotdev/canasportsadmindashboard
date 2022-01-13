547d3fd998b99eed60469357a81d9bab3e91bd1f
qudus qazeem <qudusqazeem@gmail.com>
2022-01-13 08:48:39 +0100

    Changed the Api Url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let Api = axios.create({
-    baseURL : "https://api.canasportsng.com/api",
+    baseURL : "https://api.canasportsng.com/api/public",
 });
 Api.defaults.withCredentials = true;
 export default Api;
