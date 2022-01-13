a218f57356c393a4772550941d1f217dabf99697
qudus qazeem <qudusqazeem@gmail.com>
2022-01-13 08:51:59 +0100

    Changed From VSCODE

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let Api = axios.create({
-    baseURL : "https://api.canasportsng.com/api/public",
+    baseURL : "https://api.canasportsng.com/api",
 });
 Api.defaults.withCredentials = true;
 export default Api;
